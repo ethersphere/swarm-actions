d41f2a8e6ce0fc7bb5b955f88b0099f630cd073d
Cafe137 <xcafe137@gmail.com>
2025-08-07 16:56:12 +0200

    feat: add upload-dir-experimental action

--- upload-dir-experimental/src/main.ts
+++ upload-dir-experimental/src/main.ts
@@ -13,9 +13,10 @@ type Inputs = {
 
 async function run(inputs: Inputs) {
   const bee = new Bee(inputs.beeUrl, { headers: inputs.headers })
-  const { reference, tagUid } = await bee.uploadFilesFromDirectory(
+  const { reference, tagUid } = await bee.streamDirectory(
     inputs.postageBatchId,
     inputs.dir,
+    (progress) => core.info(`Upload progress: ${progress.processed} / ${progress.total}`),
     inputs.options,
     inputs.requestOptions
   )
@@ -33,11 +34,8 @@ async function main() {
 
   const options: CollectionUploadOptions = {
     deferred: Types.asOptional((x) => Types.asBoolean(x, { name: 'deferred' }), core.getInput('deferred')),
-    encrypt: Types.asOptional((x) => Types.asBoolean(x, { name: 'encrypt' }), core.getInput('encrypt')),
     errorDocument: core.getInput('error-document'),
     indexDocument: core.getInput('index-document'),
-    pin: Types.asOptional((x) => Types.asBoolean(x, { name: 'pin' }), core.getInput('pin')),
-    tag: Types.asOptional((x) => Types.asNumber(x, { name: 'tag' }), core.getInput('tag')),
   }
 
   Objects.removeEmptyValues(options as Record<string, unknown>)
